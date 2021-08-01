200695c58b5e67e6ec37101f298e5cc81236c554
Gust4voSales <manoel0gustavo@gmail.com>
2021-07-31 21:52:03 -0300

    build: add changes to package.json and tsconfig

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
+require('dotenv').config()
+import 'express-async-errors'
 import express from 'express'
 import cors from 'cors'
 import router from './router'
-import { PORT, } from './config/env'
 import ErrorHandler from './middlewares/ErrorHandler'
 
 const app = express()
@@ -12,6 +13,7 @@ app.use(express.json())
 app.use(router)
 app.use(ErrorHandler)
 
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log("SERVER RUNNING ON PORT " + PORT)
 })
